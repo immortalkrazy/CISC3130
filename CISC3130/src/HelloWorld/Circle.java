6fa4edd7ee5ccf189451277b10f9b80b903fbef0
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2019-02-28 21:38:13 -0500

    test 1

--- CISC3130/src/HelloWorld/Circle.java
+++ CISC3130/src/HelloWorld/Circle.java
@@ -16,9 +16,6 @@ public class Circle {
 		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
