c5a39d1599a2bdc6c834d5769710e3dfc108fa3a
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2019-03-01 21:46:47 -0500

    test

--- CISC3130/src/HelloWorld/Circle.java
+++ CISC3130/src/HelloWorld/Circle.java
@@ -1,28 +1,18 @@
-/**
- * 
- */
 package HelloWorld;
 
-/**
- * @author faisal ibrahim
- *	yes i think it is working
- * i dont know
- */
 public class Circle {
 
-	/**
-	 * 
-	 */
 	public Circle() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("hello");
 		System.out.println("han  g");
 		System.out.println(" yeh kia ho raha hai");
 		System.out.println(" ");
+		
 	}
 	
 }
