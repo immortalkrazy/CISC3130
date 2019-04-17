68e11cde9323a225f8d99affe1322549ef985b40
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2019-04-16 21:43:33 -0400

    some syntex changes

--- DataStructures/src/HashMap/Employee.java
+++ DataStructures/src/HashMap/Employee.java
@@ -10,38 +10,23 @@ public class Employee {
             this.empId = empId;
             this.empName = empName;
       }
-
-      /**
-       * @return the empId
-       */
+      
       public int getEmpId() {
             return empId;
       }
 
-      /**
-       * @param empId the empId to set
-       */
       public void setEmpId(int empId) {
             this.empId = empId;
       }
 
-      /**
-       * @return the empName
-       */
       public String getEmpName() {
             return empName;
       }
 
-      /**
-       * @param empName the empName to set
-       */
       public void setEmpName(String empName) {
             this.empName = empName;
       }
 
-      /* (non-Javadoc)
-       * @see java.lang.Object#toString()
-       */
       @Override
       public String toString() {
             return "Employee [empId=" + empId + ", empName=" + empName + "]";
@@ -69,6 +54,6 @@ public class Employee {
             return true;
       }
       
-      
+     
 
 }
