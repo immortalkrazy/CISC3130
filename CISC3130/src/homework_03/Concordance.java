c724ad429efd0774ce433f97f30e544be40df0b3
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2019-05-08 19:22:02 -0400

    hw3

--- CISC3130/src/homework_03/Concordance.java
+++ CISC3130/src/homework_03/Concordance.java
@@ -1,19 +1,47 @@
 package homework_03;
 
+import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Concordance {
       
-      Map<String, LinkedList<Token>> myMap = new TreeMap<String, LinkedList<Token>>();
+      private String file;      
+      private Map<String, ArrayList<Token>> myMap;
+     
+      public Concordance() {
+            
+            myMap = new TreeMap<String, ArrayList<Token>>();
+            
+      }
       
-      public boolean add() {
+      public boolean add() throws FileNotFoundException {
+            
+            Tokenizer tz = new Tokenizer(file);
+            
+            while (tz.hasNext()) {
+                  
+                  Token temp = tz.next();
+                  
+                  if (!myMap.containsKey(temp.getWord())) {
+                        
+                       myMap.put(temp.getWord(), new ArrayList<Token>());
+                       
+                  }
+                  else {
+                        
+                        myMap.get(temp.getWord()).add(temp);                        
+                  }
+                  
+            }
             
             return true;
+            
       }
 
       public int count (String s) {
             
-            return 0;
+          return  myMap.get(s).size();
+            
       }
       
 }
--- CISC3130/src/homework_03/Tokenizer.java
+++ CISC3130/src/homework_03/Tokenizer.java
@@ -3,48 +3,50 @@ package homework_03;
 import java.io.*;
 import java.util.*;
 
-public class Tokenizer implements Iterator<String>{
+public class Tokenizer implements Iterator<Token>{
       
-      private ArrayList<Token> tokensArray = new ArrayList<Token>();
+      private int place;
+      private String file;
+      private Scanner sc;
+      private File fileName;
+      
+//      private ArrayList<Token> tokensArray = new ArrayList<Token>();
       
       public Tokenizer(String file) throws FileNotFoundException {
             
-            int place = 0;
-            File fileName = new File(file);
+            place = 0;
+            this.file = file;
+            fileName = new File(file);
+            sc = new Scanner(fileName);
             
-            try (Scanner scanner = new Scanner(fileName)) {
-                  
-                  while(scanner.hasNext()) {
-                        
-                        String word = scanner.next();
-                        place++;
-                        tokensArray.add(new Token(file, word, place));
-                
-                  }
-            }          
+            
+//            try (Scanner scanner = new Scanner(fileName)) {
+//                  
+//                  while(scanner.hasNext()) {
+//                        
+//                        String word = scanner.next();
+//                        place++;
+//                        tokensArray.add(new Token(file, word, place));
+//                
+//                  }
+//            }          
       }
 
       @Override
       public boolean hasNext() {
             
-            return false;
+            return sc.hasNext();
       }
 
       @Override
-      public String next() {
+      public Token next() {
             
-            return null;
-      }
-      
-      public void printWords() {
-            
-            for (Token t: tokensArray) {
-                  
-                  System.out.println(t);                  
-            }
+            String word = sc.next();
+            place++;
+            return new Token(file, word, place);
             
       }
       
-      
+            
 
 }
