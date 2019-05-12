c3f7b0031e61aab0907744221277014fbd900b91
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2019-05-12 15:48:25 -0400

    hw4 begins

--- CISC3130/src/homework_04/myMap.java
+++ CISC3130/src/homework_04/myMap.java
@@ -1,18 +1,31 @@
 package homework_04;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Set;
 
 public class myMap {
       
+      private ArrayList<LinkedList<Pair>> theTable;
+      private int numElements;
+      
+      // constructor for the map
+      public myMap() {
+            theTable = new ArrayList<LinkedList<Pair>>();
+            numElements = 0;
+      }
+            
       public void put(String key, Integer value) {
             
       }
       
       public boolean containsKey(String key) {
+            
             return false;
       }
       
-      public boolean conatinsValue(Integer valueInteger ) {
+      public boolean conatinsValue(Integer value) {
+            
             return false;
       }
       
@@ -30,34 +43,66 @@ public class myMap {
       
       private static class Pair {
             
+            // private data fields for pair object
             private String str;
             private Integer value;
             
+            // constructor which takes the string and a value
             public Pair(String str, Integer value) {
                   
                   this.str = str;
                   this.value = value;
             }
-
+            // returns the String key stored in the pair object
             public String getKey() {
                   return str;
             }
-
+            // returns the value stored pair object
             public Integer getValue() {
                   return value;
             }
 
-            public Integer setValue(Integer value) {
-                  this.value = value;
-                  return value;
+            public Integer setValue(Integer val) {
+                  // get the old value from the object
+                  Integer oldValue = this.value;
+                  // change value of the object with the given value
+                  this.value = val;
+                  // return old value
+                  return oldValue;
             }
+            @Override
+            public int hashCode() {
+                  final int prime = 31;
+                  int result = 1;
+                  result = prime * result + ((str == null) ? 0 : str.hashCode());
+                  return result;
+            }
+            @Override
+            public boolean equals(Object obj) {
+                  if (this == obj)
+                        return true;
+                  if (obj == null)
+                        return false;
+                  if (getClass() != obj.getClass())
+                        return false;
+                  Pair other = (Pair) obj;
+                  if (str == null) {
+                        if (other.str != null)
+                              return false;
+                  } else if (!str.equals(other.str))
+                        return false;
+                  return true;
+            } 
             
             
+      }// end of private static inner pair class
+      
+      private int getIndex(String key) {
             
-            
+            return (new Pair(key, 0)).hashCode();
             
       }
       
       
 
-}
+} // end of class
