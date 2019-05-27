4cf71c18b224fc91ae815c18b79f1c463221c013
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2019-05-27 09:12:51 -0400

    moved data structure packages into cisc3130

---
+++ CISC3130/src/Stacks/MyStack.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package Stacks;
+
+/**
+ * @author faisal
+ *
+ */
+public interface MyStack<E> {
+      
+      public boolean empty();
+      
+      public void push (E item);
+      
+      public E pop();
+      
+      public E peek();
+      	
+
+}
