d921fda3f975748478028bcdcda42f80acee9a1f
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2019-10-30 18:46:37 -0400

    test after the repo name change

--- CISC3130/src/Lists/ArrayList/Test.java
+++ CISC3130/src/Lists/ArrayList/Test.java
@@ -3,7 +3,8 @@ package Lists.ArrayList;
 public class Test {
 
       public static void main(String[] args) {
-            // TODO Auto-generated method stub
+
+            System.out.println("test");
 
       }
 
